ab47cb30771c7482f007aa87a43fd887a3a54ce5
Jared Shipley <jared.shipley@siemens.com>
2024-10-26 10:58:22 +0200

    update nuget package generation

--- CM/GenerateNugetPackage.bat
+++ CM/GenerateNugetPackage.bat
@@ -16,24 +16,30 @@ echo Create NuSpec file
 if EXIST %nugetDir%\interface.nuspec erase /f /q %nugetDir%\interface.nuspec
 if EXIST %nugetDir%\webserver.nuspec erase /f /q %nugetDir%\webserver.nuspec
 
+copy /y %rootdir%\LICENSE %nugetDir%\LICENSE.txt
+
 REM Interface Package
 echo ^<?xml version="1.0" encoding="utf-8"?^> >> %nugetDir%\interface.nuspec
 echo ^<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd"^> >> %nugetDir%\interface.nuspec
 echo ^<metadata^> >> %nugetDir%\interface.nuspec
-echo ^<id^>ModulynInterface^</id^> >> %nugetDir%\interface.nuspec
+echo ^<id^>Modulyn.Interface^</id^> >> %nugetDir%\interface.nuspec
 echo ^<version^>%version%^</version^> >> %nugetDir%\interface.nuspec
 echo ^<description^>A library to generate modules for the Modulyn Server^</description^> >> %nugetDir%\interface.nuspec
 echo ^<authors^>Jared Shipley^</authors^> >> %nugetDir%\interface.nuspec
 echo ^<repository type="git" url="https://github.com/OrgShipjd2001/ModulynBlazor.git" /^> >> %nugetDir%\interface.nuspec
 echo ^<readme^>docs\README.md^</readme^> >> %nugetDir%\interface.nuspec
+echo ^<license type="file"^>LICENSE.txt^</license^>  >> %nugetDir%\interface.nuspec
 echo ^<dependencies^> >> %nugetDir%\interface.nuspec
-echo ^<group targetFramework=".NET8.0" /^> >> %nugetDir%\interface.nuspec
+echo ^<group targetFramework="net8.0"^> >> %nugetDir%\interface.nuspec
+echo ^<dependency id="Radzen.Blazor" version="5.3.1" /^> >> %nugetDir%\interface.nuspec
+echo ^</group^> >> %nugetDir%\interface.nuspec
 echo ^</dependencies^> >> %nugetDir%\interface.nuspec
 echo ^</metadata^> >> %nugetDir%\interface.nuspec
 echo ^<files^> >> %nugetDir%\interface.nuspec
 
 echo ^<file src="%targetDir%\ModulynInterface\**" target="lib\net8.0"/^> >> %nugetDir%\interface.nuspec
 echo ^<file src="%rootDir%\README.md" target="docs\" /^> >> %nugetDir%\interface.nuspec
+echo ^<file src="%nugetdir%\LICENSE.txt" target="" /^> >> %nugetDir%\interface.nuspec
 
 echo ^</files^> >> %nugetDir%\interface.nuspec
 echo ^</package^> >> %nugetDir%\interface.nuspec
@@ -43,14 +49,17 @@ REM Deployment Package
 echo ^<?xml version="1.0" encoding="utf-8"?^> >> %nugetDir%\webserver.nuspec
 echo ^<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd"^> >> %nugetDir%\webserver.nuspec
 echo ^<metadata^> >> %nugetDir%\webserver.nuspec
-echo ^<id^>ModulynServer^</id^> >> %nugetDir%\webserver.nuspec
+echo ^<id^>Modulyn.Server^</id^> >> %nugetDir%\webserver.nuspec
 echo ^<version^>%version%^</version^> >> %nugetDir%\webserver.nuspec
 echo ^<description^>The Modulyn Server framework^</description^> >> %nugetDir%\webserver.nuspec
 echo ^<authors^>Jared Shipley^</authors^> >> %nugetDir%\webserver.nuspec
 echo ^<repository type="git" url="https://github.com/OrgShipjd2001/ModulynBlazor.git" /^> >> %nugetDir%\webserver.nuspec
 echo ^<readme^>docs\README.md^</readme^> >> %nugetDir%\webserver.nuspec
+echo ^<license type="file"^>LICENSE.txt^</license^>  >> %nugetDir%\webserver.nuspec
 echo ^<dependencies^> >> %nugetDir%\webserver.nuspec
-echo ^<group targetFramework=".NET8.0" /^> >> %nugetDir%\webserver.nuspec
+echo ^<group targetFramework="net8.0"^> >> %nugetDir%\webserver.nuspec
+echo ^<dependency id="Radzen.Blazor" version="5.3.1" /^> >> %nugetDir%\webserver.nuspec
+echo ^</group^> >> %nugetDir%\webserver.nuspec
 echo ^</dependencies^> >> %nugetDir%\webserver.nuspec
 
 echo ^</metadata^> >> %nugetDir%\webserver.nuspec
@@ -58,6 +67,7 @@ echo ^<files^> >> %nugetDir%\webserver.nuspec
 
 echo ^<file src="%targetDir%\ModulynServer\**" target="content"/^> >> %nugetDir%\webserver.nuspec
 echo ^<file src="%rootDir%\README.md" target="docs\" /^> >> %nugetDir%\webserver.nuspec
+echo ^<file src="%nugetdir%\LICENSE.txt" target="" /^> >> %nugetDir%\webserver.nuspec
 
 echo ^</files^> >> %nugetDir%\webserver.nuspec
 echo ^</package^> >> %nugetDir%\webserver.nuspec
@@ -66,5 +76,8 @@ echo Generate Nuget package
 erase /f /q %nugetDir%\*.nupkg
 %nugetexe% pack %nugetDir%\interface.nuspec -OutputDirectory %nugetDir%
 %nugetexe% pack %nugetDir%\webserver.nuspec -OutputDirectory %nugetDir%
+
+%nugetexe% source |find "dev"
+if "%ERRORLEVEL%"=="0" %nugetexe% push %nugetDir%\*.nupkg -Source Dev
 
 :Done
