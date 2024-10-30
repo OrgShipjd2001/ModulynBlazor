0a2515de8dcf34ce6da3582bf6bde9d6ea4148ee
Jared Shipley <jared.shipley@siemens.com>
2024-10-30 10:03:02 +0100

    add prerelease variable

--- CM/GenerateNugetPackage.bat
+++ CM/GenerateNugetPackage.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set prerelese=-alpha
 set scriptDir=%~dp0
 set rootDir=%scriptDir%..
 set publishDir=%rootDir%\Publish
@@ -23,7 +24,7 @@ echo ^<?xml version="1.0" encoding="utf-8"?^> >> %nugetDir%\interface.nuspec
 echo ^<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd"^> >> %nugetDir%\interface.nuspec
 echo ^<metadata^> >> %nugetDir%\interface.nuspec
 echo ^<id^>Modulyn.Interface^</id^> >> %nugetDir%\interface.nuspec
-echo ^<version^>%version%^</version^> >> %nugetDir%\interface.nuspec
+echo ^<version^>%version%%prerelease%^</version^> >> %nugetDir%\interface.nuspec
 echo ^<description^>A library to generate modules for the Modulyn Server^</description^> >> %nugetDir%\interface.nuspec
 echo ^<authors^>Jared Shipley^</authors^> >> %nugetDir%\interface.nuspec
 echo ^<repository type="git" url="https://github.com/OrgShipjd2001/ModulynBlazor.git" /^> >> %nugetDir%\interface.nuspec
@@ -50,7 +51,7 @@ echo ^<?xml version="1.0" encoding="utf-8"?^> >> %nugetDir%\webserver.nuspec
 echo ^<package xmlns="http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd"^> >> %nugetDir%\webserver.nuspec
 echo ^<metadata^> >> %nugetDir%\webserver.nuspec
 echo ^<id^>Modulyn.Server^</id^> >> %nugetDir%\webserver.nuspec
-echo ^<version^>%version%^</version^> >> %nugetDir%\webserver.nuspec
+echo ^<version^>%version%%prerelease%^</version^> >> %nugetDir%\webserver.nuspec
 echo ^<description^>The Modulyn Server framework^</description^> >> %nugetDir%\webserver.nuspec
 echo ^<authors^>Jared Shipley^</authors^> >> %nugetDir%\webserver.nuspec
 echo ^<repository type="git" url="https://github.com/OrgShipjd2001/ModulynBlazor.git" /^> >> %nugetDir%\webserver.nuspec
@@ -59,13 +60,14 @@ echo ^<license type="file"^>LICENSE.txt^</license^>  >> %nugetDir%\webserver.nus
 echo ^<dependencies^> >> %nugetDir%\webserver.nuspec
 echo ^<group targetFramework="net8.0"^> >> %nugetDir%\webserver.nuspec
 echo ^<dependency id="Radzen.Blazor" version="5.3.1" /^> >> %nugetDir%\webserver.nuspec
+echo ^<dependency id="Microsoft.EntityFrameworkCore.SqlServer" version="8.0.10" /^> >> %nugetDir%\webserver.nuspec
 echo ^</group^> >> %nugetDir%\webserver.nuspec
 echo ^</dependencies^> >> %nugetDir%\webserver.nuspec
 
 echo ^</metadata^> >> %nugetDir%\webserver.nuspec
 echo ^<files^> >> %nugetDir%\webserver.nuspec
 
-echo ^<file src="%targetDir%\ModulynServer\**" target="content"/^> >> %nugetDir%\webserver.nuspec
+echo ^<file src="%targetDir%\ModulynServer\**" target="content" exclude="**\microsoft*.*;**\system*.*;**\radzen*.*;**\Azure*.*" /^> >> %nugetDir%\webserver.nuspec
 echo ^<file src="%rootDir%\README.md" target="docs\" /^> >> %nugetDir%\webserver.nuspec
 echo ^<file src="%nugetdir%\LICENSE.txt" target="" /^> >> %nugetDir%\webserver.nuspec
 
