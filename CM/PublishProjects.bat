e5ce1f470e353209f8b0c4cbbc45db3e6aeeb653
Jared Shipley <jared.shipley@siemens.com>
2025-01-15 18:21:16 +0100

    update packages

--- CM/PublishProjects.bat
+++ CM/PublishProjects.bat
@@ -11,11 +11,11 @@ if exist %PublishDir% rmdir /s /q %PublishDir%
 pushd %rootdir%
 
 dotnet publish ModulynServer\ModulynServer.csproj -o %pubDebDir%\ModulynServer --no-self-contained -c Debug -r win-x64
-dotnet publish ModulynInterface\ModulynInterface.csproj -o %pubDebDir%\ModulynInterface --no-self-contained -c Debug -r win-x64
+dotnet publish ModulynInterface\ModulynInterface.csproj -o %pubDebDir%\ModulynInterface --no-self-contained -c Debug
 dotnet publish Test\TestModule\TestModule.csproj -o %pubDebDir%\Modules\TestModule --no-self-contained -c Debug -r win-x64
 
 dotnet publish ModulynServer\ModulynServer.csproj -o %pubRelDir%\ModulynServer --no-self-contained -c Release -r win-x64
-dotnet publish ModulynInterface\ModulynInterface.csproj -o %pubRelDir%\ModulynInterface --no-self-contained -c Release -r win-x64
+dotnet publish ModulynInterface\ModulynInterface.csproj -o %pubRelDir%\ModulynInterface --no-self-contained -c Release
 dotnet publish Test\TestModule\TestModule.csproj -o %pubRelDir%\Modules\TestModule --no-self-contained -c Release -r win-x64
 
 popd
