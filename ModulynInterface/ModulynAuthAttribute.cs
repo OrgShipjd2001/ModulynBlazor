6fd73ad49ebb9db398323284cc707366ad9224da
Jared Shipley <jared.shipley@siemens.com>
2025-06-28 20:56:40 +0200

    Authorization is also working

--- ModulynInterface/ModulynAuthAttribute.cs
+++ ModulynInterface/ModulynAuthAttribute.cs
@@ -6,11 +6,14 @@ namespace Modulyn.Server.Interface
     [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public class ModulynAuthAttribute : AuthorizeAttribute
     {
+        private const string PolicyPrefix = "PageAccessPolicy:";
+
         public ModulynAuthRole Role { get; set; }
 
         public ModulynAuthAttribute(ModulynAuthRole role)
         {
             Role = role;
+            Policy = $"{PolicyPrefix}{role.ToString()}";
         }
     }
 }
