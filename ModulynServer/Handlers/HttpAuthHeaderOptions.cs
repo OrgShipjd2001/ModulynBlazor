eaaba8451891a3dcf45602e8a63077dcb0a84bc7
Jared Shipley <jared.shipley@siemens.com>
2025-11-03 16:56:26 +0100

    add missing files

---
+++ ModulynServer/Handlers/HttpAuthHeaderOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace ModulynServer.Handlers
+{
+    public class HttpAuthHeaderOptions : AuthenticationSchemeOptions
+    {
+        /// <summary>
+        /// The HTTP header that contains the authenticated username.
+        /// </summary>
+        public string UserHeader { get; set; } = "X-User";
+
+        /// <summary>
+        /// Optional HTTP header that contains the user’s email address.
+        /// </summary>
+        public string? EmailHeader { get; set; } = "X-Email";
+    }
+}
